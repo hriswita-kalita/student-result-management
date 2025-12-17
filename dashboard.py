88ce5a73cd75232a27d7c00e7bd743dcf32a6a2a
annon-git <hriswitakalita77@gmail.com>
2025-12-17 17:31:34 +0530

    Add dashboard navigation buttons

--- dashboard.py
+++ dashboard.py
@@ -19,6 +19,11 @@ class RMS:
         M_Frame.place(x=10, y=70, width=1340, height=80)
 
         btn_course = Button(M_Frame, text = "Course", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x=20, y=5, width = 200, height = 40) 
+        btn_student = Button(M_Frame, text = "Student", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x=240, y=5, width = 200, height = 40)
+        btn_result = Button(M_Frame, text = "Result", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x=460, y=5, width = 200, height = 40)
+        btn_view = Button(M_Frame, text = "View Student Result", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x=680, y=5, width = 200, height = 40)
+        btn_logout = Button(M_Frame, text = "Logout", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x=900, y=5, width = 200, height = 40)
+        btn_exit = Button(M_Frame, text = "Exit", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x=1120, y=5, width = 200, height = 40)   
 
 if __name__ == "__main__":
     root = Tk()
