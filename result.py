7f88655fd400054562df2186cefe457a8b7be718
annon-git <hriswitakalita77@gmail.com>
2025-12-18 20:12:30 +0530

    Add variables for result form

--- result.py
+++ result.py
@@ -15,4 +15,10 @@ class ResultClass:
         Label(self.root, text="Add Student Result", font=("goudy old style", 20, "bold"),
               bg="#ff9800").place(x=10, y=15, relwidth=1180, height=35)
         
-        
+
+        # Variables
+        self.var_student = StringVar()
+        self.var_name = StringVar()
+        self.var_course = StringVar()
+        self.var_marks = StringVar()
+        self.var_full_marks = StringVar()
