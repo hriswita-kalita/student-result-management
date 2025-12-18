f98dd2adbcbd87e9c0bb82d49516812fa7ef7f66
annon-git <hriswitakalita77@gmail.com>
2025-12-18 21:02:12 +0530

    Add View Student Results title banner

--- report.py
+++ report.py
@@ -6,3 +6,6 @@ class ReportClass:
     def __init__(self, root):
         self.root=root; self.root.title("Result Management System"); self.root.geometry("1200x650+80+80"); self.root.config(bg="white")
 
+# Title
+Label(self.root,text="View Student Results",font=("goudy old style",30,"bold"),bg="#f7a400",fg="black").place(x=0,y=0,relwidth=1,height=70)
+
