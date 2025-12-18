e7c940835d9b0b172782763000af8a88a3ca2922
annon-git <hriswitakalita77@gmail.com>
2025-12-18 20:08:31 +0530

    Create result module structure

---
+++ result.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+from PIL import Image, ImageTk
+import sqlite3
+
+class ResultClass:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Student Result Management System")
+        self.root.geometry("1200x700+80+170")
+        self.root.config(bg="white")
+        self.root.focus_force()
