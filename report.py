a25f4dd096633cb5997a5e07a78c7322de5e7381
annon-git <hriswitakalita77@gmail.com>
2025-12-18 21:01:43 +0530

    Initialize ReportClass window and imports

--- report.py
+++ report.py
@@ -1,13 +1,8 @@
 from tkinter import *
-from tkinter import ttk, messagebox
+from tkinter import messagebox
 import sqlite3
 
 class ReportClass:
     def __init__(self, root):
-        self.root = root
-        self.root.title("Result Management System")
-        self.root.geometry("1200x700+80+170")
-        self.root.config(bg="white")
-        self.root.focus_force()
-
+        self.root=root; self.root.title("Result Management System"); self.root.geometry("1200x650+80+80"); self.root.config(bg="white")
 
