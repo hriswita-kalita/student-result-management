6ef43bf06b54a38784518352a536193eaaa656cf
annon-git <hriswitakalita77@gmail.com>
2025-12-18 19:29:24 +0530

    Add student form variables

--- student.py
+++ student.py
@@ -14,4 +14,16 @@ class studentClass:
         title = Label(self.root, text="Manage Student Details", font=("goudy old style", 20, "bold"), bg="#033054", fg="white")
         title.place(x=10, y=15, relwidth=1180, height=35)
 
-        
+        # Variables
+        self.var_roll = StringVar()
+        self.var_name = StringVar()
+        self.var_email = StringVar()
+        self.var_gender = StringVar()
+        self.var_dob = StringVar()
+        self.var_contact = StringVar()
+        self.var_course = StringVar()
+        self.var_admission = StringVar()
+        self.var_state = StringVar()
+        self.var_city = StringVar()
+        self.var_pin = StringVar()
+        self.var_search = StringVar()
